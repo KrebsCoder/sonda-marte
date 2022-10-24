723440d3ca09a3a937e6f3f41d8dea6169d736fd
KrebsCoder <lucasklopess@gmail.com>
2022-10-24 16:30:16 -0300

    add: new directories including the start of the project

---
+++ back-end/src/test/java/com/example/sondamarte/SondaMarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sondamarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondaMarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
