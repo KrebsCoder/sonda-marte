76c5ca1c75647b745ff4ffa3ff80c6cbad8edd9f
KrebsCoder <lucasklopess@gmail.com>
2022-10-26 20:06:42 -0300

    feat: protected findAll method and new methods to move probe

--- probe/src/main/java/com/api/sondamarte/services/ProbeService.java
+++ probe/src/main/java/com/api/sondamarte/services/ProbeService.java
@@ -5,6 +5,7 @@ import com.api.sondamarte.models.PlanetModel;
 import com.api.sondamarte.models.ProbeModel;
 import com.api.sondamarte.repositories.PlanetRepository;
 import com.api.sondamarte.repositories.ProbeRepository;
+import com.api.sondamarte.enums.ProbeDirection;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -44,8 +45,12 @@ public class ProbeService {
         return probeRepository.save(probe);
     }
 
-    public List<ProbeModel> findAll() {
-        return probeRepository.findAll();
+    public ResponseEntity<Object> findAll() {
+        Optional<List<ProbeModel>> optionalProbeModelList = Optional.of(probeRepository.findAll());
+        if (optionalProbeModelList.get().isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Probe not found.");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(optionalProbeModelList.get());
     }
     public ResponseEntity<Object> findByName(String name) {
         Optional<ProbeModel> probeModelOptional = probeRepository.findByName(name);
@@ -57,7 +62,7 @@ public class ProbeService {
 
     @Transactional
     public List<ProbeModel> deleteAll() {
-        List<ProbeModel> probes = findAll();
+        List<ProbeModel> probes = probeRepository.findAll();
         for (ProbeModel probe : probes){
             probeRepository.deleteByName(probe.getName());
         }
@@ -93,5 +98,91 @@ public class ProbeService {
                 optionalPlanetModel.get());
 
         return ResponseEntity.status(HttpStatus.CREATED).body(save(probeModel));
+    }
+
+    public ResponseEntity<Object> changeProbeDirection(ProbeDto probeDto) {
+        Optional<ProbeModel> optionalProbeModel = probeRepository.findByName(probeDto.getName());
+        if (optionalProbeModel.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Probe not found.");
+        }
+        for (int i = 0; i < probeDto.getMovement().length(); i++){
+            if (probeDto.getMovement().charAt(i) == 'L'){
+                handleMovementLeft(optionalProbeModel.get());
+            } else if (probeDto.getMovement().charAt(i) == 'R'){
+                handleMovementRight(optionalProbeModel.get());
+            } else if (probeDto.getMovement().charAt(i) == 'M'){
+                handleMovement(optionalProbeModel.get());
+            }
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(save(optionalProbeModel.get()));
+    }
+
+    private void handleMovement(ProbeModel probeModel) {
+
+        switch (probeModel.getDirection()){
+            case NORTH -> moveProbeNorth(probeModel);
+            case SOUTH -> moveProbeSouth(probeModel);
+            case EAST -> moveProbeEast(probeModel);
+            case WEST -> moveProbeWest(probeModel);
+        }
+    }
+
+    private void moveProbeWest(ProbeModel probeModel) {
+        int x = probeModel.getPositionX();
+
+        if (probeModel.getPositionX() == 0){
+            probeModel.changePositionX(probeModel.getPlanet().getSizeX());
+        } else {
+            probeModel.decreasePositionX();
+        }
+    }
+
+    private void moveProbeEast(ProbeModel probeModel) {
+        int x = probeModel.getPositionX();
+
+        if (probeModel.getPositionX() == probeModel.getPlanet().getSizeX()){
+            probeModel.changePositionX(0);
+        } else {
+            probeModel.increasePositionX();
+        }
+    }
+
+    private void moveProbeSouth(ProbeModel probeModel) {
+        int y = probeModel.getPositionY();
+
+        if (probeModel.getPositionY() == 0){
+            probeModel.changePositionY(probeModel.getPlanet().getSizeY());
+        } else {
+            probeModel.decreasePositionY();
+        }
+    }
+
+    private void moveProbeNorth(ProbeModel probeModel) {
+        int y = probeModel.getPositionY();
+
+        if (probeModel.getPositionY() == probeModel.getPlanet().getSizeY()){
+            probeModel.changePositionY(0);
+        } else {
+            probeModel.increasePositionY();
+        }
+    }
+
+
+    private void handleMovementRight(ProbeModel probeModel) {
+        switch (probeModel.getDirection()){
+            case NORTH -> probeModel.changeDirection(probeModel, ProbeDirection.EAST);
+            case SOUTH -> probeModel.changeDirection(probeModel, ProbeDirection.WEST);
+            case EAST -> probeModel.changeDirection(probeModel, ProbeDirection.SOUTH);
+            case WEST -> probeModel.changeDirection(probeModel, ProbeDirection.NORTH);
+        }
+    }
+
+    private void handleMovementLeft(ProbeModel probeModel) {
+        switch (probeModel.getDirection()) {
+            case NORTH -> probeModel.changeDirection(probeModel, ProbeDirection.WEST);
+            case SOUTH -> probeModel.changeDirection(probeModel, ProbeDirection.EAST);
+            case EAST -> probeModel.changeDirection(probeModel, ProbeDirection.NORTH);
+            case WEST -> probeModel.changeDirection(probeModel, ProbeDirection.SOUTH);
+        }
     }
 }
