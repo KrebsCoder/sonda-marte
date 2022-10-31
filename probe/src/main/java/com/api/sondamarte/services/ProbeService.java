64f9b812868b327592b6155bcbf90be85a3bafac
KrebsCoder <lucasklopess@gmail.com>
2022-10-31 15:29:50 -0300

    fix: now you can move to your previous position

--- probe/src/main/java/com/api/sondamarte/services/ProbeService.java
+++ probe/src/main/java/com/api/sondamarte/services/ProbeService.java
@@ -46,7 +46,7 @@ public class ProbeService {
         List<ProbeModel> probeModelList = planet.getProbes();
 
         for (ProbeModel probe : probeModelList){
-            if (!validateProbePosXPosY(probe, posX, posY)){
+            if (!validateProbeCreationPosXPosY(probe, posX, posY)){
                 return false;
             }
         }
@@ -160,11 +160,22 @@ public class ProbeService {
         return true;
     }
 
+    private boolean validateProbeCreationPosXPosY(ProbeModel probeModel, int posX, int posY) {
+        List<ProbeModel> probesList = probeModel.getPlanet().getProbes();
+
+        for (ProbeModel probe : probesList){
+            if (posX == probe.getPositionX() && posY == probe.getPositionY()){
+                return false;
+            }
+        }
+        return true;
+    }
+    
     private boolean validateProbePosXPosY(ProbeModel probeModel, int posX, int posY) {
         List<ProbeModel> probesList = probeModel.getPlanet().getProbes();
 
         for (ProbeModel probe : probesList){
-            if ((probe != probeModel) &&  posX == probe.getPositionX() && posY == probe.getPositionY()){
+            if ((probe != probeModel) &&posX == probe.getPositionX() && posY == probe.getPositionY()){
                 return false;
             }
         }
