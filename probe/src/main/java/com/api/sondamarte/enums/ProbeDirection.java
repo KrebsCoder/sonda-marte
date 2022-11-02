f63cd43a43276d2fd302f2588d2c752271324408
KrebsCoder <lucasklopess@gmail.com>
2022-11-02 13:56:41 -0300

    feat: added new methods to enum class

--- probe/src/main/java/com/api/sondamarte/enums/ProbeDirection.java
+++ probe/src/main/java/com/api/sondamarte/enums/ProbeDirection.java
@@ -1,8 +1,50 @@
 package com.api.sondamarte.enums;
 
 public enum ProbeDirection {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST
+    NORTH{
+        @Override
+        public ProbeDirection right() {
+            return EAST;
+        }
+        @Override
+        public  ProbeDirection left(){
+            return WEST;
+        }
+    },
+    SOUTH{
+        @Override
+        public ProbeDirection right() {
+            return WEST;
+        }
+
+        @Override
+        public ProbeDirection left() {
+            return EAST;
+        }
+    },
+    WEST{
+        @Override
+        public ProbeDirection right() {
+            return NORTH;
+        }
+
+        @Override
+        public ProbeDirection left() {
+            return SOUTH;
+        }
+    },
+    EAST{
+        @Override
+        public ProbeDirection right() {
+            return SOUTH;
+        }
+
+        @Override
+        public ProbeDirection left() {
+            return NORTH;
+        }
+    };
+
+    public abstract ProbeDirection right();
+    public abstract ProbeDirection left();
 }
